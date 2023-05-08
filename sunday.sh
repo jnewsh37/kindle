c3e32810738ee3274b09ea4c15ecfb2b6a479462
Jack <nikolanewsham@gmail.com>
2023-05-07 18:31:38 -1000

    Added Sunday scripts.

---
+++ sunday.sh
@@ -0,0 +1,14 @@
+./fetchcomic.sh
+./datesunday.sh
+rm joinedcomics.png
+convert bignate.png \
+	-resize 530x800 \
+	bignate.png
+convert +append -gravity center Date2.png bignate.png joinedcomics.png 
+rm bignate.png
+rm Date2.png
+# send it to the kindle and display it
+echo "sending to kindle..."
+scp -q joinedcomics.png kindle:. 2> /dev/null
+ssh kindle "/usr/sbin/eips -c; /usr/sbin/eips -g ./joinedcomics.png" 2> /dev/null
+echo "done"
