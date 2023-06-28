fd7dd1d903066fee1310785e6623647c556c341e
Jack <jack@elitedesk.home>
2023-06-27 15:02:57 -1000

    Minor Modifications

--- battery.sh
+++ battery.sh
@@ -1,1 +1,1 @@
-ssh kindle "cd /; ./usr/bin/dm.sh; cd /mnt/us/documents; batteryinfovar=$(grep "battinfo" all_system_logs* | tail -1); blevelvar=$(echo $batteryinfovar|cut -f4 -d " "); echo $blevelvar"
+ssh kindle "cd /; rm /mnt/us/documents/all_*; rm /mnt/us/documents/wininfo*; ./usr/bin/dm.sh; cd /mnt/us/documents; batteryinfovar=$(grep "battinfo" all_system_logs* | tail -1); blevelvar=$(echo $batteryinfovar|cut -f4 -d " "); echo $blevelvar"
