8a39e588ff88cfd49e1217f0f97ece36344cbdf2
Jack Newsham <nikolanewsham@gmail.com>
2023-11-08 14:44:43 -1000

    added battery icon

--- convert.sh
+++ convert.sh
@@ -1,6 +1,8 @@
 oldname=$(basename -s .jpeg $1)
 name=$oldname"converted"
 
+echo "Converting"
+
 convert $1 \
         -rotate 270 \
         -filter LanczosSharp \
@@ -13,3 +15,9 @@ convert $1 \
         -define png:color-type=0 \
         -define png:bit-depth=8 \
         $name.png
+
+echo "sending to kindle..."
+scp -q $name.png kindle:. 2> /dev/null
+ssh kindle "/usr/sbin/eips -c; /usr/sbin/eips -g ./$name.png" 2> /dev/null
+rm $name.png
+
