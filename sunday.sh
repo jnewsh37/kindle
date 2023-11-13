e5461364eba7a693b310b4ba1c3d776c3c4eed3e
Jack Newsham <nikolanewsham@gmail.com>
2023-11-12 22:02:12 -1000

    added alt image for when text banners are displayed

--- sunday.sh
+++ sunday.sh
@@ -11,15 +11,18 @@ fi
 ./datesunday.sh
 rm joinedcomics.png
 convert $COMIC.png \
-	-resize 530x800 \
+	-resize 800x453 \
 	$COMIC.png
 convert +append -gravity center Date2.png $COMIC.png joinedcomics.png 
 COMIC=bignate
 convert -rotate 90 joinedcomics.png joinedcomics.png
+convert $COMIC.png -rotate 90 -resize 800x453 alt.png
+convert -append -gravity center blankstrip.png alt.png alt.png
 rm $COMIC.png
 rm Date2.png
 # send it to the kindle and display it
 echo "sending to kindle..."
 scp -q joinedcomics.png kindle:. 2> /dev/null
+scp -q alt.png kindle:. 2> /dev/null
 ssh kindle "/usr/sbin/eips -c; /usr/sbin/eips -g ./joinedcomics.png" 2> /dev/null
 echo "done"
