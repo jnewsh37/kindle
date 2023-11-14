e1c5915136b63a00004fd295047e9764b25d3268
Jack Newsham <nikolanewsham@gmail.com>
2023-11-13 15:33:59 -1000

    temporary change

--- textmessage.sh
+++ textmessage.sh
@@ -3,10 +3,11 @@ convert -background white \
         -fill black  \
         -font Palatino-Bold \
         -pointsize 36 \
-        -size 624x147 \
+        -size 800x147 \
 	-gravity Center \
         caption:" $message" \
         messagetext.png
 
 scp -q messagetext.png kindle:. 2> /dev/null
 ssh kindle "/usr/sbin/eips -g ./messagetext.png" 2> /dev/null
+#624x147
