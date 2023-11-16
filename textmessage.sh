38864cdd8b87e2ad1bd01f3f551d7d5516f9e23c
Jack Newsham <nikolanewsham@gmail.com>
2023-11-15 19:33:55 -1000

    squashed some bugs

--- textmessage.sh
+++ textmessage.sh
@@ -1,16 +1,19 @@
-message=$(echo $1|cut  -c 1-100)
+cd `dirname $0`
+message="$(echo $1|cut  -c 1-100)"
 convert -background white \
         -fill black  \
         -font Palatino-Bold \
         -pointsize 36 \
         -size 690x147 \
-	-gravity Center \
+        -gravity Center \
         caption:" $message" \
-	-define png:color-type=0 \
+        -define png:color-type=0 \
         -define png:bit-depth=8 \
         messagetext.png
-echo $2
+#echo $2
+convert +append ./"${2}.png" messagetext.png messagetext.png
 scp -q messagetext.png kindle:. 2> /dev/null
 ssh kindle "/usr/sbin/eips -g ./messagetext.png" 2> /dev/null
 #624x147
 #176x147
+
