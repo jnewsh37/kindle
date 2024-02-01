fc72f3b7b282be7287e55de074b517019e985966
Jack <nikolanewsham@gmail.com>
2024-01-31 16:00:12 -1000

    fixed stuff

--- bothcomics.sh
+++ bothcomics.sh
@@ -10,15 +10,14 @@ rm -f joinedcomics.png
 convert +append dateweather.png $1.png $2.png joinedcomics.png
 #convert +append chargekindleicon.png garfield.png bignate.png comicscharge.png
 convert -rotate 90 joinedcomics.png joinedcomics.png
-<<<<<<< HEAD
 echo "saving comics to disk..."
 mv ../Comics/*.png ../Comics/Vault/
-mv bignate.png ../Comics/mkindle/"$1$DATE".png
-mv garfield.png ../Comics/mkindle/"$2$DATE".png
+mv bignate.png ../Comics/kindle/"$1$DATE".png
+mv garfield.png ../Comics/kindle/"$2$DATE".png
 cp joinedcomics.png ../Comics/"comics$DATE".png
 rm Date.png
 # send it to the kindle and display it
 echo "sending to kindle..."
-scp -q joinedcomics.png mkindle:. 2> /dev/null
-ssh mkindle "/usr/sbin/eips -c; sleep 1; /usr/sbin/eips -vg ./joinedcomics.png; sleep 1; /usr/sbin/eips -g ./joinedcomics.png" 2> /dev/null
+scp -q joinedcomics.png kindle:. 2> /dev/null
+ssh kindle "/usr/sbin/eips -c; sleep 1; /usr/sbin/eips -vg ./joinedcomics.png; sleep 1; /usr/sbin/eips -g ./joinedcomics.png" 2> /dev/null
 echo "done"
