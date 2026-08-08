ff009f63b5c3a6e296884709019cebc3b73d51ed
Jack Newsham <nikolanewsham@gmail.com>
2026-08-07 16:19:44 -1000

    Added game end behavior

--- sports/Sports.py
+++ sports/Sports.py
@@ -12,11 +12,18 @@ class game:
 		self.score2 = runCommand('jq', '-r', f'.events[{self.num}].competitions[0].competitors[1].score', './mlb.txt').strip()
 		return f"{self.team1}: {self.score1} {self.team2}: {self.score2}"
 	def getLastPlay(self):
-		self.playDesc = runCommand('jq', '-r', f'.events[{self.num}].competitions[0].situation.lastPlay.text', './mlb.txt').strip()
-		return self.playDesc
+		print(runCommand('jq', f'.events[{self.num}].competitions[].playByPlayAvailable', './mlb.txt'))
+		if (runCommand('jq', f'.events[{self.num}].competitions[].situation', './mlb.txt').strip() != "null"):
+			self.playDesc = runCommand('jq', '-r', f'.events[{self.num}].competitions[0].situation.lastPlay.text', './mlb.txt').strip()
+			return self.playDesc
+		else:
+			return " "
 	def getCount(self):
-		self.count = f"Balls: {runCommand('jq', '-r', f'.events[{self.num}].competitions[0].situation.balls', './mlb.txt').strip()} Strikes: {runCommand('jq', '-r', f'.events[{self.num}].competitions[0].situation.strikes', './mlb.txt').strip()}"
-		return self.count
+		if (runCommand('jq', f'.events[{self.num}].competitions[].situation', './mlb.txt').strip() != "null"):
+			self.count = f"Balls: {runCommand('jq', '-r', f'.events[{self.num}].competitions[0].situation.balls', './mlb.txt').strip()} Strikes: {runCommand('jq', '-r', f'.events[{self.num}].competitions[0].situation.strikes', './mlb.txt').strip()}"
+			return self.count
+		else:
+			return "Game end"
 
 	
 
@@ -42,35 +49,40 @@ def addToQueue(line, row, info):
 runCommand('curl', "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard", '--output', 'mlb.txt')
 runCommand('ssh', 'kindle', '/usr/sbin/eips', '-fc')
 
-spacing = 5
-yspacing = 3
-
-for i in range(5):
-	addToQueue((yspacing + i*spacing), 3, f'{games[f"{gamelist[i]}"].getScore()}')
-	addToQueue((yspacing+1 + i*spacing), 3, f'{games[f"{gamelist[i]}"].getCount()}')
-	print(gamelist[i])
-	pbp = games[f"{gamelist[i]}"].getLastPlay()
-	if (len(pbp) > 26):
-		pbp1 = pbp[:(pbp.rfind(' ', 0, 26))]
-		pbp2 = pbp[(len(pbp1)+1):]
-		addToQueue((yspacing+2 + i*spacing), 3, ("  " + pbp1))
-		addToQueue((yspacing+3 + i*spacing), 3, ("  " + pbp2))
-	else:
-		addToQueue((yspacing+2 + i*spacing), 3, f'  {games[f"{gamelist[i]}"].getLastPlay()}')
-
-for i in range(5):
-	addToQueue((yspacing + i*spacing), 35, f'{games[f"{gamelist[i+3]}"].getScore()}')
-	addToQueue((yspacing+1 + i*spacing), 35, f'{games[f"{gamelist[i+3]}"].getCount()}')
-	print(gamelist[i])
-	pbp = games[f"{gamelist[i+3]}"].getLastPlay()
-	if (len(pbp) > 26):
+def run10():
+	spacing = 5
+	yspacing = 3
+	runCommand('ssh', 'kindle', '/usr/sbin/eips', '-c')
+	for i in range(5):
+		addToQueue((yspacing + i*spacing), 3, f'{games[f"{gamelist[i]}"].getScore()}')
+		addToQueue((yspacing+1 + i*spacing), 3, f'{games[f"{gamelist[i]}"].getCount()}')
+#		print(gamelist[i])
+		pbp = games[f"{gamelist[i]}"].getLastPlay()
+		if (len(pbp) > 26):
 			pbp1 = pbp[:(pbp.rfind(' ', 0, 26))]
 			pbp2 = pbp[(len(pbp1)+1):]
-			addToQueue((yspacing+2 + i*spacing), 35, ("  " + pbp1))
-			addToQueue((yspacing+3 + i*spacing), 35, ("  " + pbp2))
-	else:	
-			addToQueue((yspacing+2 + i*spacing), 35, f'  {games[f"{gamelist[i+3]}"].getLastPlay()}')
-	
-runCommand('ssh', 'kindle', ";".join(writeQueue))
-runCommand('curl', "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard", '--output', 'mlb.txt')
-writeQueue = []
+			addToQueue((yspacing+2 + i*spacing), 3, ("  " + pbp1))
+			addToQueue((yspacing+3 + i*spacing), 3, ("  " + pbp2))
+		else:
+			addToQueue((yspacing+2 + i*spacing), 3, f'  {games[f"{gamelist[i]}"].getLastPlay()}')
+
+	for i in range(5):
+		addToQueue((yspacing + i*spacing), 35, f'{games[f"{gamelist[i+3]}"].getScore()}')
+		addToQueue((yspacing+1 + i*spacing), 35, f'{games[f"{gamelist[i+3]}"].getCount()}')
+#		print(gamelist[i])
+		pbp = games[f"{gamelist[i+3]}"].getLastPlay()
+		if (len(pbp) > 26):
+				pbp1 = pbp[:(pbp.rfind(' ', 0, 26))]
+				pbp2 = pbp[(len(pbp1)+1):]
+				addToQueue((yspacing+2 + i*spacing), 35, ("  " + pbp1))
+				addToQueue((yspacing+3 + i*spacing), 35, ("  " + pbp2))
+		else:	
+				addToQueue((yspacing+2 + i*spacing), 35, f'  {games[f"{gamelist[i+3]}"].getLastPlay()}')
+		
+	runCommand('ssh', 'kindle', ";".join(writeQueue))
+	runCommand('curl', "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard", '--output', 'mlb.txt')
+	writeQueue.clear()
+
+while True:
+	run10()
+	time.sleep(60)
