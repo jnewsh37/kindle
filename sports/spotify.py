850aaced95128e13d9ba787d88b8168bcd53c0a8
Jack Newsham <nikolanewsham@gmail.com>
2026-09-23 12:25:24 -0400

    spotify.py now displays the current lyric line of the song actively playing, and renders a progress bar showing timestamp and time remaining of the song

--- sports/spotify.py
+++ sports/spotify.py
@@ -3,13 +3,13 @@
 import sys, syncedlyrics, subprocess, spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyPKCE
 from PIL import Image, ImageDraw, ImageFont
-from datetime import timedelta
+from datetime import timedelta, datetime
 from basketball import runCommand
 
 SCOPE = "user-read-currently-playing"
 redirect = "http://127.0.0.1:8888/callback"
 
-# Creates a spotipy.Spotify client, 
+# Creates a spotipy.Spotify
 def create_spotify_client(scope = SCOPE) -> spotipy.Spotify:
     ("Authenticating using OAuth")
     auth_manager = SpotifyOAuth(client_id=sys.argv[1], client_secret=sys.argv[2], redirect_uri=redirect, scope=scope)
@@ -31,6 +31,29 @@ def fontSize(f):
 	global font
 	font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial.ttf", f)
 
+def pullLyrics(name, artist) -> list:
+     return str(syncedlyrics.search(f"[{artist}] [{name}]")).split("\n")
+
+def getLyrics(lyrics, position):
+    lyricsList = lyrics
+    lyricsDict = {}
+    for l in lyricsList:
+         lyricsDict[str(l)[1:9]] = str(l)[11:(len(l))]   
+    times = list(lyricsDict.keys())
+    delta = -1
+    for t in times:
+        tList = t.split(":")
+        newT = (int(tList[0])*60 + int(float(tList[1])))*1000
+        if (abs(newT-position)<delta or delta == -1):
+            line = lyricsDict[t]
+            delta = abs(newT-position)
+        t = newT
+
+    if delta >= 0:
+         return line
+    else:
+         return "***"  
+
 def renderPlaying(client:spotipy.Spotify):
     data = client.currently_playing()
     name = data["item"]["name"]
@@ -38,20 +61,36 @@ def renderPlaying(client:spotipy.Spotify):
     for artist in data["item"]["artists"]:
         artists.append(artist["name"])
     progress = data["progress_ms"]
+    length = data["item"]["duration_ms"]
     coverURL = str(data["item"]["album"]["images"][0]["url"])
     cover = (coverURL.split("/"))[-1] + ".jpeg"
     runCommand("curl", coverURL, "--output", cover)
 
+    lyrics = pullLyrics(name, artists[0])
+    currentLine = getLyrics(lyrics, progress)
+
     screen = Image.new("L", (800,600), 255)
     draw = ImageDraw.Draw(screen)
-    pasteImage(50, 100, 400, screen, cover)
+    imgSize = 300
+    pasteImage(50, 50, imgSize, screen, cover)
     fontSize(30)
-    draw.text((500, 100), name,font=font)
+    draw.text((100+imgSize, 100), name,font=font)
     fontSize(20)
-    draw.text((500, 150), artists[0],font=font)
+    draw.text((100+imgSize, 150), artists[0],font=font)
+
+    if len(currentLine) > 30:
+         currentLine = f"{currentLine[0:30]}\n{currentLine[30:len(currentLine)]}"
+    fontSize(50)
+    draw.text((50, 100+imgSize), currentLine, font=font)
+
+# Progress bar
+    draw.line((150, 550, 700, 550), fill=100, width=10)
+    draw.line((150, 550, 150+(progress/length*550), 550), fill=0, width=10)
+    fontSize(25)
+    draw.text((115, 550), ((str(timedelta(milliseconds=progress))[2:7])), font=font, align="center", anchor="mm")
 
     screen.save("SpotifyTest.png")
-    print(f"Song: {name}\nArtists: {artists}\nCover image URL: {coverURL}\nProgress: {progress}")
+    print(f"Song: {name}\nArtists: {artists}\nCover image URL: {coverURL}\nProgress: {int(progress/60000)}:{int((progress%60000)/1000)}\nCurrent lyrics: {currentLine}")
 
 renderPlaying(create_spotify_client())
 runCommand("scp", "SpotifyTest.png", "kindle3:~/")
